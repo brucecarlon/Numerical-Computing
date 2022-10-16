b6417c96105504063aa56693a116a54911bb213d
brucecarlon <cmvubele@gmail.com>
2022-10-16 18:14:26 +0200

    initial commit

---
+++ Newton_Raphson_method/main.py
@@ -0,0 +1,35 @@
+def newton_raphson(function, derivative, x, threshold, maxiter):
+    """
+    Given a function, its derivative, initial guess, threshold between two
+    consecutive guesses and max number of iterations: finds the closest root
+    to the initial guess
+
+    return values are the root found and the number of iterations to find root
+    otherwise return None value if no roots are found
+
+    """
+    try:
+        for i in range(maxiter):
+            x_new = x - function(x) / derivative(x)
+            if i == maxiter - 1:
+                return None
+            if abs(x_new - x) < threshold:
+                break
+            else:
+                x = x_new
+
+        return round(x_new, 3), i
+    except OverflowError:
+        print("Error: Please check that your parameters are correct")
+
+
+if __name__ == "__main__":
+    # =============================================================================
+    # y = lambda x: 2 * x**3 - 9.5 * x + 7.5
+    # dy = lambda x: 6 * x**2 - 9.5
+    #
+    # =============================================================================
+
+    y = lambda x: x**3 + 1
+    dy = lambda x: 3 * x
+    print(newton_raphson(y, dy, -10, 0.0001, 100))
